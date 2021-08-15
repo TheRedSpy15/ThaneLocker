71b81ec9f2d125079b3c7fcfab6592ec42b001e9
TheRedSpy15 <hjadar15@protonmail.com>
2021-08-14 21:46:20 -0400

    added margins to buttons in news

--- app/src/main/java/com/theredspy15/thanelocker/ui/news/NewsFragment.java
+++ app/src/main/java/com/theredspy15/thanelocker/ui/news/NewsFragment.java
@@ -1,14 +1,13 @@
 package com.theredspy15.thanelocker.ui.news;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -60,20 +59,16 @@ public class NewsFragment extends Fragment {
 
     public void displayEntries(List<SyndEntry> entries) {
         if (entries != null) {
-            GradientDrawable shape = new GradientDrawable();
-            shape.setShape(GradientDrawable.RECTANGLE);
-            shape.setColor(Color.RED);
-            shape.setStroke(3, Color.YELLOW);
-            shape.setCornerRadius(15);
             for (SyndEntry entry: entries) {
                 Button button = new Button(getContext());
                 button.setText(entry.getTitle());
                 button.setTextSize(18);
                 button.setPadding(50,50,50,50);
-                button.setBackground(shape);
                 button.setBackgroundColor(getResources().getColor(R.color.grey));
                 button.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(entry.getUri()))));
-                binding.feedLayout.addView(button);
+                LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                layout.setMargins(0,20,0,20);
+                binding.feedLayout.addView(button, layout);
             }
         }
     }
