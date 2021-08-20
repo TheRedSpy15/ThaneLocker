7218c9607bbdf3739818cf0e3de5705c443006a1
TheRedSpy15 <hjdadar15@protonmail.com>
2021-08-20 03:32:47 -0400

    little start with osmdroid

--- app/src/main/java/com/theredspy15/thanelocker/ui/mainfragments/skatemap/SkateMapFragment.java
+++ app/src/main/java/com/theredspy15/thanelocker/ui/mainfragments/skatemap/SkateMapFragment.java
@@ -1,5 +1,6 @@
 package com.theredspy15.thanelocker.ui.mainfragments.skatemap;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 
+import com.example.thanelocker.R;
 import com.example.thanelocker.databinding.FragmentSkatemapBinding;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
 
 public class SkateMapFragment extends Fragment {
 
     private SkateMapViewModel skatemapViewModel;
     private FragmentSkatemapBinding binding;
+
+    private MapView map = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +34,43 @@ public class SkateMapFragment extends Fragment {
         binding = FragmentSkatemapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = requireContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
+        //tile servers will get you banned based on this string
+
+        map = binding.maplayout.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(5.0);
+
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
