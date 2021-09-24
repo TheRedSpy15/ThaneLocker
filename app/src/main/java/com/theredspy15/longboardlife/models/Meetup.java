7869d49e3a207c3f08b02714c24b8bd695b4c28f
TheRedSpy15 <hjadar15@protonmail.com>
2021-09-24 02:01:42 -0400

    finished renaming package

--- app/src/main/java/com/theredspy15/longboardlife/models/Meetup.java
+++ app/src/main/java/com/theredspy15/longboardlife/models/Meetup.java
@@ -5,12 +5,17 @@ import java.util.Date;
 import java.util.List;
 
 public class Meetup {
+    private int meet_id = 0;
     private String title;
     private String description;
-    private List<Integer> profileIds = new ArrayList<>(); // people attending
+    private List<Integer> attending_ids = new ArrayList<>(); // people attending
     private double latitude;
     private double longitude;
     private Date date;
+
+    public static void loadFromFirebase() {
+
+    }
 
     public String getTitle() {
         return title;
@@ -28,12 +33,12 @@ public class Meetup {
         this.description = description;
     }
 
-    public List<Integer> getProfileIds() {
-        return profileIds;
+    public List<Integer> getAttending_ids() {
+        return attending_ids;
     }
 
-    public void setProfileIds(List<Integer> profileIds) {
-        this.profileIds = profileIds;
+    public void setAttending_ids(List<Integer> attending_ids) {
+        this.attending_ids = attending_ids;
     }
 
     public double getLatitude() {
@@ -58,5 +63,13 @@ public class Meetup {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public int getMeet_id() {
+        return meet_id;
+    }
+
+    public void setMeet_id(int meet_id) {
+        this.meet_id = meet_id;
     }
 }
